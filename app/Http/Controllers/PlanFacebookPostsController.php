e3085f6c02052a43d97d3c737cb7745c2e9fdcf4
psalmistcom <dadewale9@gmail.com>
2024-01-26 09:44:32 +0100

    facebook and instagram posts controller

--- app/Http/Controllers/PlanFacebookPostsController.php
+++ app/Http/Controllers/PlanFacebookPostsController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\planfacebookposts;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PlanFacebookPostsController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $facebook_posts = planfacebookposts::paginate(20);
+        if ($facebook_posts) {
+            return response()->json($facebook_posts);
+        } else return response()->json('no facebook posts');
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'post_date' => 'required',
+            'text' => 'required',
+            'post_id_from_facebook' => 'required',
+            'image_link' => 'required',
+            'post_link' => 'required'
+        ]);
+
+        planfacebookposts::create($request->all());
+        return response()->json('facebook post created');
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $facebook_post = planfacebookposts::find($id);
+        if ($facebook_post) {
+            $facebook_post->delete();
+            return response()->json('facebook post is deleted');
+        } else {
+            return response()->json('facebook post not found');
+        }
+    }
 }
--- app/Http/Controllers/PlanInstagramPostsController.php
+++ app/Http/Controllers/PlanInstagramPostsController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\planinstagramposts;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PlanInstagramPostsController extends Controller
 {
-    //
+    public function index(): JsonResponse
+    {
+        $instagram_posts = planinstagramposts::paginate(20);
+        if ($instagram_posts) {
+            return response()->json($instagram_posts);
+        } else return response()->json('no instagram posts');
+    }
+
+    public function store(Request $request): JsonResponse
+    {
+        $this->validate($request, [
+            'post_date' => 'required',
+            'text' => 'required',
+            'post_id_from_instagram' => 'required',
+            'image_link' => 'required',
+            'post_link' => 'required'
+        ]);
+
+        planinstagramposts::create($request->all());
+        return response()->json('instagram post created');
+    }
+
+    public function destroy($id): JsonResponse
+    {
+        $instagram_post = planinstagramposts::find($id);
+        if ($instagram_post) {
+            $instagram_post->delete();
+            return response()->json('instagram post is deleted');
+        } else {
+            return response()->json('instagram post not found');
+        }
+    }
 }
